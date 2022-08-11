8cfbe34d8c84466a0154afe96b5794779fe020f1
Ekevoo <x@ekevoo.com>
2022-08-11 00:28:22 -0400

    manifest adjustments, not happy with it yet

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
+@echo off
+rem consider migrating to syso:
+rem https://pkg.go.dev/github.com/hallazzang/syso
+
+echo on
+rsrc -manifest windows.manifest -ico check.ico
 go build -ldflags="-H windowsgui"
